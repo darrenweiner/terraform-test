b5dd8073c20992d49eb0ede38f12e04536d0ba87
Darren Weiner <darren.weiner@S04635-MBPR.local>
2019-10-29 14:31:21 -0600

    initial aws

--- aws.tf
+++ aws.tf
@@ -1,5 +1,11 @@
+variable "access_key" {
+  type = string
+}
+variable "secret_key" {
+  type = string
+}
 provider "aws" {
   region     = "us-east-1"
-  access_key = "${access_key}"
-  secret_key = "${secret_key}"
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
 }
